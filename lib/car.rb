68a66661f4dcf4c6cb752c1d3c70d3e828abec47
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-06-11 02:05:03 +0300

    commit tree 7c78586257a576fe8d1408ad68cf22805b694408

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,12 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+
+class Car < Vehicle
+    def go #overwrite
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end  
+end
+
+puts Car.superclass
+p Car.ancestors 
+p Car.superclass.superclass.superclass.superclass
